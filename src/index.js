58454c3f0009a18a1960552b23a27678ecaadb99
HarshBhargava <harshbhargava16@gmail.com>
2025-07-24 12:54:00 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import connectDB from './db/database.js';
+import connectDB from "./db/database.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 
-dotenv.config({})
-connectDB();
+dotenv.config({});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Is Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Data Base Connection failed", error.message);
+  });
