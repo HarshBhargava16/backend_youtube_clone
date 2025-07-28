0bc9f546c265b0a3496c15977882a6d04359b463
HarshBhargava <harshbhargava16@gmail.com>
2025-07-28 12:09:46 +0530

    fix env file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+
+dotenv.config();
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
@@ -17,8 +20,11 @@ const uploadOnCloudinary = async (localFilePath) => {
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_key,
+  api_secret: process.env.CLOUDINARY_secret,
 });
 
+
+
 export { uploadOnCloudinary };
+
